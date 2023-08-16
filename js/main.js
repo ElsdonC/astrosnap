31e312edf596cd2980d8424b713be0e9bceb9d36
ElsdonC <elsdonc21@gmail.com>
2023-08-15 18:05:55 -0700

    rebranded and changed api endpoints

--- js/main.js
+++ js/main.js
@@ -1,49 +1,42 @@
-const gelokyKEY = config.GELOKY_KEY
 const nasaKEY = config.NASA_KEY
 
+// Retreive Current Date
+var date = new Date();
+var year = date.toLocaleString("default", { year: "numeric" });
+var month = date.toLocaleString("default", { month: "2-digit" });
+var day = date.toLocaleString("default", { day: "2-digit" });
+var currentDate = year + "-" + month + "-" + day;
+document.getElementById("date").value = currentDate;
+getImage()
+
+// Display Explanation When Info Icon is Hovered
+document.getElementById("info-icon").addEventListener("mouseover", () => {
+  document.getElementById("explanation").style.display = "block";
+})
+document.getElementById("info-icon").addEventListener("mouseleave", () => {
+  document.getElementById("explanation").style.display = "none";
+})
+
+// Handle Generate Image Form Submission
 document.querySelector('form').addEventListener("submit", (e) => {
   e.preventDefault()
-  getLatLon()
+  getImage()
 })
 
-document.querySelector('#reset').addEventListener("click", () => {
-  document.querySelector("#location").value = ""
-  document.querySelector("img").src = ""
-  document.querySelector("h2").innerHTML = ""
-  document.querySelector("#reset").style.display = "none";
-})
-
-async function getLatLon() {
-  let lon
-  let lat
-  const address = document.querySelector('#location').value
-  const gelokyURL = `https://geloky.com/api/geo/geocode?address=${address}&key=${gelokyKEY}&format=geloky`
-  
-  await fetch(gelokyURL)
-    .then(res => res.json())
-    .then(data => {
-      lon = Number(data[0].longitude)
-      lat = Number(data[0].latitude)
-      document.querySelector("h2").innerText = `Image Loading...!`
-      getImage(lon, lat, address)
-      document.querySelector("#reset").style.display = "block";
-    })
-    .catch(err => {
-      console.error(err)
-      alert("You entered an invalid address, try again!")
-      document.querySelector('#location').value = ""
-    })
-}
-
-async function getImage(lon, lat, address) {
-  const nasaURL = `https://api.nasa.gov/planetary/earth/imagery?lon=${lon}&lat=${lat}&dim=0.10&date=2020-11-01&api_key=${nasaKEY}`
-  await fetch(nasaURL)
-    .then(res => {
-      document.querySelector("h2").innerText = `This is a Satellite Image from Nasa at ${address}`
-      document.querySelector("img").src = res.url
-    })
-    .catch(err => {
-      console.error(err)
-    })
+// Get Image from NASA API & Fill Page with Data Retrieved
+async function getImage() {
+  const date = document.getElementById("date").value;
+  const nasaURL = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${nasaKEY}`
+  const res = await fetch(nasaURL);
+  const data = await res.json();
+  if (data.title === undefined) {
+    document.getElementById("title").innerText = "Picture Not Found, Try Another Date!"
+    document.getElementById("explanation").innerText = "Picture Not Found, Try Another Date!"
+    document.querySelector("img").src = data.url
+  } else {
+    document.getElementById("title").innerText = data.title
+    document.getElementById("explanation").innerText = data.explanation
+    document.querySelector("img").src = data.url
+  }
   
 }
